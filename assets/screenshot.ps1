ba480215c746db0492851972571b9c2b2f3b5334
Malty <126196145+KazoxXii@users.noreply.github.com>
2026-06-09 14:59:45 +0200

    Add files via upload

---
+++ assets/screenshot.ps1
@@ -0,0 +1,11 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+$bitmap = New-Object System.Drawing.Bitmap($screen.Width, $screen.Height)
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphics.CopyFromScreen($screen.Location, [System.Drawing.Point]::Empty, $screen.Size)
+$bitmap.Save("C:\Users\Administrateur\Documents\SITE WEB\website\assets\photo-vitrine.png")
+$graphics.Dispose()
+$bitmap.Dispose()
+Write-Host "Done"
